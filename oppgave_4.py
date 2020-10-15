183b18f35ab8070bfa5b717e22fb0351ef184c9d
NikolaiDokken <nikolaidokken@live.no>
2020-10-15 13:58:44 +0200

    oppg4

--- oppgave_4.py
+++ oppgave_4.py
@@ -1,43 +1,43 @@
 import numpy as np
 import scipy
+from oppgave_1 import exp
+
+# Har:
+# Dreiemoment L
+# Treghetsmoment I (matrise)
+# W_i
+# Har exp() fra oppgave 1a)
+
+# Trenger:
+# Σ_i
+
+def litenSigmaTilStor(liten):
+    return np.array([[0, -liten[2], liten[1]], [liten[2], 0, -liten[0]], [-liten[1], liten[0], 0]])
 
 
-I = np.array([[1,0,0],[0,1,0],[0,0,1]])
-L = np.array([[]])
-rotasjonsVektorw = np.array([1,1,1])
-treghetsmomentI = 3
-dreiemomentL = np.dot(treghetsmomentI, rotasjonsVektorw)
-
-def getSigma(sigmaVector):
-    pass
-
-def RK4_iteration(W):
-    sigma1 = np.linalg.inv(I)*W.transpose()*dreiemomentL
-
-
-print(I.transpose())
-
-# GEEKS FOR GEEKS
-# Finds value of y for a given x using step size h
-# and initial value y0 at x0.
-
-
-def rungeKutta(x0, y0, x, h):
+def rungeKutta(x0, W0, x, h):
     # Count number of iterations using step size or
     # step height h
     n = (int)((x - x0)/h)
     # Iterate for number of iterations
-    y = y0
+    W = W0
+
+    I = np.array([[1,0,0],[0,1,0],[0,0,1]]) # need proper data
+    L = np.dot(I, [1,1,1]) # Need proper data
     for i in range(1, n + 1):
         "Apply Runge Kutta Formulas to find next value of y"
-        k1 = h * dydx(x0, y)
-        k2 = h * dydx(x0 + 0.5 * h, y + 0.5 * k1)
-        k3 = h * dydx(x0 + 0.5 * h, y + 0.5 * k2)
-        k4 = h * dydx(x0 + h, y + k3)
+        k1 = np.linalg.inv(I)*W.transpose()*L
+        k2 = np.linalg.inv(I)*exp(litenSigmaTilStor(k1),-h/2)*W.transpose()*L
+        k3 = np.linalg.inv(I)*exp(litenSigmaTilStor(k2), -h/2)*W.transpose()*L
+        k4 = np.linalg.inv(I)*exp(litenSigmaTilStor(k3), -h)*W.transpose()*L
 
         # Update next value of y
-        y = y + (1.0 / 6.0)*(k1 + 2 * k2 + 2 * k3 + k4)
+        W = W*exp(litenSigmaTilStor(k1) + 2*litenSigmaTilStor(k2) + 2*litenSigmaTilStor(k3) + litenSigmaTilStor(k4), h/6)
 
         # Update next value of x
         x0 = x0 + h
-    return y
+    return W
+
+if __name__ == "__main__":
+  testLiten = np.array([2, 3, 4])
+  print(litenSigmaTilStor(testLiten))
