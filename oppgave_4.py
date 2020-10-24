125ebd77191c0b77d17476424632ba2ca90153e7
NikolaiDokken <nikolaidokken@live.no>
2020-10-24 16:18:35 +0200

    oppg 4,5 and 6

--- oppgave_4.py
+++ oppgave_4.py
@@ -1,16 +1,5 @@
 import numpy as np
-import scipy
 from oppgave_1 import exp
-
-# Har:
-# Dreiemoment L
-# Treghetsmoment I (matrise)
-# W_i
-# Har exp() fra oppgave 1a)
-
-# Trenger:
-# Σ_i
-
 
 def liten_sigma_til_stor(liten):
     return np.array([[0, -liten[2], liten[1]],
@@ -26,20 +15,17 @@ def rungeKutta(x0, treghetsmoment, dreiemoment, h):
     treg_inv = np.linalg.inv(treghetsmoment)
     W_trans = W.transpose()
 
-    "Apply Runge Kutta Formulas to find next value of y"
-
+    "Apply Runge Kutta Formulas to find next value of W"
     k1 = np.matmul(np.matmul(treg_inv, W_trans), dreiemoment)
-    k2 = np.matmul(np.matmul(np.matmul(treg_inv, exp(liten_sigma_til_stor(k1),-h/2)),W_trans),dreiemoment)
-    k3 = np.matmul(np.matmul(np.matmul(treg_inv, exp(liten_sigma_til_stor(k2), -h / 2)), W_trans), dreiemoment)
-    k4 = np.matmul(np.matmul(np.matmul(treg_inv, exp(liten_sigma_til_stor(k3), -h)), W_trans), dreiemoment)
+    k2 = np.matmul(np.matmul(np.matmul(treg_inv, exp(liten_sigma_til_stor(k1),-h/2)),W_trans),
+                   dreiemoment)
+    k3 = np.matmul(np.matmul(np.matmul(treg_inv, exp(liten_sigma_til_stor(k2), -h / 2)), W_trans),
+                   dreiemoment)
+    k4 = np.matmul(np.matmul(np.matmul(treg_inv, exp(liten_sigma_til_stor(k3), -h)), W_trans),
+                   dreiemoment)
 
     # Update next value of W
     W = np.matmul(W, exp(liten_sigma_til_stor(k1) + 2*liten_sigma_til_stor(k2) +
             2*liten_sigma_til_stor(k3) + liten_sigma_til_stor(k4),h/6))
 
     return W
-
-
-if __name__ == "__main__":
-    testLiten = np.array([2, 3, 4])
-    print(liten_sigma_til_stor(testLiten))
---
+++ oppgave_6.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from oppgave_1 import treghetsmoment
+import numpy as np
+from oppgave_4 import rungeKutta
+
+
+x0 = np.identity(3)
+treghetsmoment = treghetsmoment()
+
+rotasjonsvektor_a = np.array([1, 0.05, 0])
+dreiemoment_a = np.matmul(treghetsmoment, rotasjonsvektor_a)
+
+plots = [[[] for x in range(3)] for y in range(3)]
+for k in range(10):
+    x_i = rungeKutta(x0, treghetsmoment, dreiemoment_a, 0.1)
+    for i in range(3):
+        for j in range(3):
+            plots[i][j].append(x_i[i][j])
+
+    x0 = x_i
+
+for i in range(3):
+    for j in range(3):
+        plt.plot(plots[i][j])
+
+plt.ylabel('some numbers')
+plt.show()
