d4983ec8524da834bf9e6f90c55ba54c777d9649
NikolaiDokken <nikolaidokken@live.no>
2020-10-25 18:48:48 +0100

    oppg 6 update

--- oppgave_6.py
+++ oppgave_6.py
@@ -11,13 +11,17 @@ rotasjonsvektor_a = np.array([1, 0.05, 0])
 dreiemoment_a = np.matmul(treghetsmoment, rotasjonsvektor_a)
 
 plots = [[[] for x in range(3)] for y in range(3)]
-for k in range(10):
-    x_i = rungeKutta(x0, treghetsmoment, dreiemoment_a, 0.1)
+
+# Interval [0‚1] with step size 0.01
+for k in range(100):
+    x_i = rungeKutta(x0, treghetsmoment, dreiemoment_a, 0.01)
     for i in range(3):
         for j in range(3):
             plots[i][j].append(x_i[i][j])
 
     x0 = x_i
+
+print(x0)
 
 for i in range(3):
     for j in range(3):
