1c482540eb328322db12cadbbc524914d6c06d31
Zenjjim <zaim.imran@gmail.com>
2020-10-27 15:00:58 +0100

    c

--- oppgave_2.py
+++ oppgave_2.py
@@ -1,6 +1,4 @@
 import numpy as np
 
-
 def X_t(t):
     return np.array([[1, 0, 0], [0, np.cos(t), -np.sin(t)], [0, np.sin(t), np.cos(t)]])
-
--- oppgave_3.py
+++ oppgave_3.py
@@ -1,19 +1,24 @@
-# Implementer varianten av Eulers metode gitt i likning (6) i avnsitt 4.1. Og test metoden p ̊a systemet som best ̊ar av likningene (3) og (4)._
+# Implementer varianten av Eulers metode gitt i likning (6) i avnsitt 4.1.
 import numpy as np
 from oppgave_1 import exp
 from oppgave_2 import X_t
+import matplotlib.pyplot as plt
 # I er treghetsmoment, W_i resultatene og L er dreiemomentet
+
+plots = [[[] for x in range(3)] for y in range(3)]
 
 
 def calculate_omega_i(I, W_i, L):
     I_inv = np.linalg.inv(I)
     W_t = np.transpose(W_i)
-    return I_inv @ W_t @ L
+    return np.matmul(np.matmul(I_inv, W_t), L)
 
 
 def Euler(h, X_0, I, L):
     W_i = X_0
-    for i in range(0, int(1/h)):
+    x_t = X_t(0)
+    find_diff(0, x_t, W_i)
+    for i in range(1, int(1/h)):
         # For all W-values after w0
         omega_i = calculate_omega_i(I, W_i, L)
         # Find Omega formel 18
@@ -22,10 +27,8 @@ def Euler(h, X_0, I, L):
                           [-omega_i[1], omega_i[0], 0]])
         # Calculate W_i+1
         W_i = np.matmul(W_i, exp(Omega, h))
-        print("###")
-        print("i:", i)
-        print(W_i)
-        print(X_t(h*i))
+        x_t = X_t(h*i)
+        find_diff(i, x_t, W_i)
     return W_i
 
 
@@ -35,5 +38,22 @@ def test():
     X_0 = indentity
     I = indentity
     W = Euler(0.25, X_0, I, L)
+    fig, axs = plt.subplots(3, 3)
+
+    axis = ['x', 'y', 'z']
+    for i in range(3):
+        for j in range(3):
+            axs[i, j].plot(plots[i][j])
+            axs[i, j].set_title(f'Axis [{axis[i]},{axis[j]}]')
+
+    plt.show()
+
+
+def find_diff(i, x_t, W_i):
+    diff = np.abs(W_i - x_t)
+    for i in range(3):
+        for j in range(3):
+            plots[i][j].append(diff[i][j])
+
 
 test()
